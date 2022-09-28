894e33fd6c7db199360a58090340f81b88d412c6
Amir Nazari <a.nazari5868@gmail.com>
2022-09-28 14:09:50 -0400

    tweet scraper v1

--- main.go
+++ main.go
@@ -1,16 +1,40 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"os"
 
 	twitterscraper "github.com/n0madic/twitter-scraper"
 )
 
 func main() {
 	scraper := twitterscraper.New()
-	tweet, err := scraper.GetTweet("1575080854623174656")
+	f, err := os.Create("tweets.txt")
+
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
-	fmt.Println(tweet.Text)
+
+	defer f.Close()
+
+	fmt.Println("done")
+	for tweet := range scraper.SearchTweets(context.Background(),
+		"برای #مهسا_امینی", 50) {
+		if tweet.Error != nil {
+			panic(tweet.Error)
+		}
+		_, err2 := f.WriteString(tweet.Text + "\n")
+
+		if err2 != nil {
+			log.Fatal(err2)
+		}
+		_, err3 := f.WriteString("-------------------------\n")
+
+		if err3 != nil {
+			log.Fatal(err3)
+		}
+		fmt.Println(tweet.Text)
+	}
 }
