27dd5fd4312cfc5802db8384875e4163f13cb4ee
v8hid <scrubby440@me.com>
2022-09-30 02:35:30 +0400

    UI update

--- web-ui/app.vue
+++ web-ui/app.vue
@@ -1,3 +1,11 @@
+<script setup>
+useHead({
+  title: "For you ...",
+  viewport: "width=device-width, initial-scale=1, maximum-scale=1",
+  charset: "utf-8",
+});
+</script>
+
 <template>
   <NuxtPage />
 </template>
